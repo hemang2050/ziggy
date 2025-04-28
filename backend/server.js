f3b42b6aebd2acf6f86b393a5614d6d2adf1a0f8
hemang2050 <hechou@iu.edu>
2025-04-28 18:25:45 -0400

    deployed backend url update

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://ziggy-frontend.vercel.app"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors({
+    origin: ['https://ziggy-frontend.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  }));
 
 app.use(express.json());
 
